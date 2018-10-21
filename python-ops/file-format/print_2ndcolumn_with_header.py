c0ecdb3f5cd8fec3d8b913f642faaa53e69b6b85
nik786 <chakraborty.rock@gmail.com>
2018-10-21 21:41:27 +0530

    changes

--- python-ops/file-format/print_2ndcolumn_with_header.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-test_file = 'test21.csv'
-csv_file = csv.DictReader(open(test_file, 'rb'), delimiter=',', quotechar='"')
-for line in csv_file:
-    print line['authors'] + " \t" + line ["price"]

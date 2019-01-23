5d9b60b398d4e2d9e3138c42e4e329b2724815f0
nik786 <chakraborty.rock@gmail.com>
2019-01-23 08:03:04 +0530

    Update an.sh

--- ansible-1/an.sh
+++ ansible-1/an.sh
@@ -2,3 +2,8 @@ ansible-playbook -i inventories/generic/static/local/ac-1.yml playbooks/jenkins-
 http://devopstechie.com/creating-custom-dynamic-inventory-with-ansible-using-python/
 https://qiita.com/t_nakayama0714/items/91c2d711a397df68dddc
 
+docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+
+docker run --name dev-pg -e POSTGRES_PASSWORD=docker123 -d postgres
+
+

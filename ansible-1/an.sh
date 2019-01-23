36bbc7f0b1c81ea15e146976728ca8a0d8e37a9a
nik786 <chakraborty.rock@gmail.com>
2019-01-23 07:35:04 +0530

    Update an.sh

--- ansible-1/an.sh
+++ ansible-1/an.sh
@@ -1,1 +1,4 @@
 ansible-playbook -i inventories/generic/static/local/ac-1.yml playbooks/jenkins-1.yml 
+http://devopstechie.com/creating-custom-dynamic-inventory-with-ansible-using-python/
+https://qiita.com/t_nakayama0714/items/91c2d711a397df68dddc
+

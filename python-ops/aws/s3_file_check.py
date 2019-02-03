64b15ee857171721edb9e880fb4166606da12d9e
nik786 <chakraborty.rock@gmail.com>
2019-02-03 21:47:57 +0530

    Update s3_file_check.py

--- python-ops/aws/s3_file_check.py
+++ python-ops/aws/s3_file_check.py
@@ -1,13 +1,24 @@
 import boto3
 import argparse
+import csv
+import datetime
+
+headers = [['FILENAME', 'LAST MODIFIED DATE', 'SIZE']]
 
 def check_object(bucket_name,key_name):
     s3_client = boto3.client('s3')
     res = s3_client.list_objects(
     Bucket=bucket_name,
     Prefix=key_name)
+
     if 'Contents' in res and  (len(res['Contents'])==1):
-        print ("{} {} {} KB".format(res['Contents'][0]['Key'], str(res['Contents'][0]['LastModified']), str(res['Contents'][0]['Size']/1024) ))
+        file_name = "{}-S3-REPORT.CSV".format(str(datetime.datetime.now().strftime('%d-%m-%y')))
+        with open(file_name, 'w') as writeFile:
+            writer = csv.writer(writeFile)
+            row = [str(res['Contents'][0]['Key']), str(res['Contents'][0]['LastModified']),str(float(res['Contents'][0]['Size'])/1024)+'KB']
+            headers.append(row)
+            writer.writerows(headers)
+            print ("{} {} {} KB".format(res['Contents'][0]['Key'], str(res['Contents'][0]['LastModified']), str(float(res['Contents'][0]['Size'])/1024) ))
     else:
         print("File Missing!!")
 

c6c65373ae15ca2543eb9f90db8b31bee022cdf9
nik786 <chakraborty.rock@gmail.com>
2019-02-23 13:37:53 +0530

    Update patch.py

--- python-ops/report/patch.py
+++ python-ops/report/patch.py
@@ -1,10 +1,31 @@
 import subprocess
 import argparse
+from datetime import datetime
 import pandas as pd
 import re
 import  json
 from  bson.json_util  import  dumps
 import plotly as py
+from email import Encoders
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.mime.text import MIMEText
+
+def copy():
+        p = subprocess.Popen("ansible-playbook -i ./inv3.sh copy.yml",shell=True, stdout=subprocess.PIPE)
+        p.wait()
+        out = p.communicate()
+def mail():
+         msg = MIMEMultipart()
+         msg["From"] = "me@example.com"
+         msg["To"] = "sudipta1436@gmail.com"
+         msg["Subject"] = "Report Ansible."
+         text="""Kindly click on below mentioned url to check patch updates\nhttp://ec2-54-159-8-36.compute-1.amazonaws.com/patch/"""
+         msg.attach(MIMEText(text))
+         p =subprocess.Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=subprocess.PIPE)
+         p.communicate(msg.as_string())
+
+
 
 def cmds(ip,usr,pwd):
         #this function will initialize all the commands and give them to run function for execution and return the output as well
@@ -19,13 +40,14 @@ def json_read_file(json_file):
         datax = json.load(f)
         keyz = datax.keys()
         status_machine=[]
+	j=0
         for i in range(0,len(keyz)):
                 try:
                                 hosts = datax[keyz[i]]["hosts"]
                                 user = datax[keyz[i]]["vars"]["ansible_ssh_user"]
                                 psw = datax[keyz[i]]["vars"]["ansible_ssh_pass"]
-				filex = open("server%s.html"%str(i+1),"w+")
 				for ip in hosts:
+					filex = open("server%s.html"%str(j+1),"w+")
 					new=[]
 					final=[]
                                         data=cmds(ip,user,psw)
@@ -51,9 +73,11 @@ def json_read_file(json_file):
 							for it in final[fl]:
 								filex.write("<td>%s</td>\n"%it)
 							filex.write("</tr>")
+					j=j+1
+					print ip
+					filex.close()
                                         continue
                 except Exception as e:
-				print e
                                 continue
 
 if __name__ == '__main__':
@@ -62,7 +86,11 @@ if __name__ == '__main__':
         parser.add_argument("-i")
         args = parser.parse_args()
         if args.i is not None:
+		startTime = datetime.now()
+		print "script started at: %s"%startTime
                 json_read_file(args.i)
+		copy()
+		mail()
+		print "script ended at: %s"%datetime.now()
         else:
-                print "Missing arguments ."
-
+                print "Missing arguments"

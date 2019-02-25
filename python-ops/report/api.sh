ed7f6d1628bd3c5ee8850d4dc333c3ab54f3e599
nik786 <chakraborty.rock@gmail.com>
2019-02-25 23:25:05 +0530

    Rename api.yml to api-2.yml

--- python-ops/report/api.sh
+++
@@ -1,1 +0,0 @@
-curl -ku xxxxx:xxxxx -H "Content-Type: application/json" -X POST -d '{"description":"Tower CLI", "application":null, "scope":"write"}' http://localhost/api/v2/users/1/personal_tokens/

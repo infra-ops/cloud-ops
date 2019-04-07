241eec3cee1b4ae11a11559c0190b4eb098ceb55
nik786 <chakraborty.rock@gmail.com>
2019-04-07 12:52:02 +0530

    schanges

--- python-ops/rest-api/curl.py
+++
@@ -1,2 +0,0 @@
-curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type:                                                                                                                                   
-application/json" -X POST http://localhost:85/

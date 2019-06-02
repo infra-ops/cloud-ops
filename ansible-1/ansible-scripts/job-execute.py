8f193eb97596c35298eaaca08ca16ffb6d27e0fc
nik786 <chakraborty.rock@gmail.com>
2019-06-02 17:16:39 +0530

    Update job-execute.py

--- ansible-1/ansible-scripts/job-execute.py
+++ ansible-1/ansible-scripts/job-execute.py
@@ -29,3 +29,23 @@ curl -i \
 
 
 
+
+
+echo $WORKSPACE
+echo $BUILD_NUMBER
+dt=`date "+%Y-%m-%d-%H-%M"`
+
+
+aws s3 cp /var/lib/jenkins/workspace/war-deploy-1/target/$war1 s3://hello-artifactory/$BUILD_NUMBER/ --recursive --acl public-read-write
+
+
+war1=`curl -s -k -u nik:iis123 "http://localhost:8085/job/war-deploy-1/lastBuild/consoleText"  | tail -n 50 | sed -n '/upload/p' | egrep -o mavenproject1-1.0-SNAPSHOT.*war | cut -d " " -f1`
+echo $war1
+
+
+curl -s -k  \
+     -u admin:password \
+     -H "Accept: application/json" \
+     -H "Content-Type:application/json" \
+     -X POST --data '{"extra_vars":{"build_no":"'${BUILD_NUMBER}'","ear_ver":"'${war1}'"}}' "http://localhost/api/v2/job_templates/21/launch/"
+

ed01a765ab99a0d23b71fdd06066bc56bceee7a4
nik786 <chakraborty.rock@gmail.com>
2020-07-18 19:23:53 +0530

    add

---
+++ ansible-1/roles/haproxy/as.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Long Running Job is beginning"
+echo "My Job is to Sleep"
+echo "Now am going to Sleep for $1 Seconds"
+sleep $1
+echo "Huuuhhh!!.. What time is It?"
+echo "Can I get some Bed Coffee please"
+exit

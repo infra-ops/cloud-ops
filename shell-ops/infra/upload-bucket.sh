81f5fe2ef2221fe389e2b3f9a8d54622d07ed648
nik786 <chakraborty.rock@gmail.com>
2021-08-10 11:18:33 +0530

    Create upload-bucket.sh

---
+++ shell-ops/infra/upload-bucket.sh
@@ -0,0 +1,35 @@
+[
+    {
+        "AllowedHeaders": [
+            "*"
+        ],
+        "AllowedMethods": [
+            "PUT",
+            "POST",
+            "DELETE"
+        ],
+        "AllowedOrigins": [
+            "*"
+        ],
+        "ExposeHeaders": [],
+        "MaxAgeSeconds": 3000
+    }
+]
+
+
+
+file=d1.jpg
+bucket=dbrick
+resource="/${bucket}/${file}"
+contentType="image/png"
+dateValue=`date -R`
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key=
+s3Secret=
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+curl -X PUT -T "${file}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  https://${bucket}.s3.amazonaws.com/${file}

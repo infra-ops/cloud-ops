e554892f1e3b47d0cd22bc1410bf18213ef07d9b
nik786 <chakraborty.rock@gmail.com>
2023-09-06 20:53:31 +0530

    Create compare_same_keys_2.sh

---
+++ shell-ops/infra/shell/compare_same_keys_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set the delimiter
+delimiter="="
+
+# Initialize an array to store keys that do not exist in the second file
+not_found=()
+
+# Read and process each line of the first file
+while IFS= read -r line; do
+    key=$(echo "$line" | awk -F"$delimiter" '{print $1}')
+    if grep -q "$key$delimiter" file2; then
+        echo "Key '$key' exists in both files."
+    fi
+done < "file1"
